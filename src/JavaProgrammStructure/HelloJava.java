e868266f6aec27e840c187258834e004bb6ecd82
“ddianaahlullina” <ahlullina.diana@gmail.com>
2022-10-20 09:42:04 +0300

    Added HelloJava.java

---
+++ src/JavaProgrammStructure/HelloJava.java
@@ -0,0 +1,8 @@
+package JavaProgrammStructure;
+
+public class HelloJava {
+    //Напишите программу, которая выводит на экран строку "Hello, Java!" (без кавычек)
+    public static void  main(String [] args){
+        System.out.println("Hello, Java!");
+    }
+}
